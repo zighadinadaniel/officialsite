7700698e7f6c61e02f7dbf10d3fe0b4aea28cb54
Zighadina Daniel <122740337+zighadinadaniel@users.noreply.github.com>
2023-07-14 12:14:46 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,120 @@
+// javascript
+const items=[
+{
+    id:1,
+    name:"Ash Varsity Jacket",
+    colors:{
+        1:"Black",
+        2:"White"
+    },
+    image:"images/image0 (6).jpeg",
+    price:35
+    
+},
+
+
+{
+    id:2,
+    name:"Tattic Jacket",
+    colors:{
+        1:"Black",
+        2:"White"
+    },
+    image:"images/image4.jpeg",
+    price:32
+    
+},
+
+
+{
+    id:3,
+    name:"Atlete fashion Jacket",
+    colors:{
+        1:"Black",
+        2:"Brown"
+    },
+    image:"images/image1 (2).jpeg",
+    price:75
+    
+},
+
+{
+    id:4,
+    name:"Camo Jacket",
+    colors:{
+        1:"Army Green"
+    },
+    image:"images/image0 (7).jpeg",
+    price:35
+    
+},
+
+
+{
+    id:4,
+    name:"Tattic Sleeveless Jacket",
+    colors:{
+        1:"Brown"
+    },
+    image:"images/tattic-sleeveless.jpg",
+    price:50
+    
+},
+
+{
+    id:5,
+    name:"Plain Pants",
+    colors:{
+        1:"Brown"
+    },
+    image:"images/1.jpeg",
+    price:50
+    
+},
+
+{
+    id:6,
+    name:"Ash Pants",
+    colors:{
+        1:"Black",
+        2:"White",
+        3:"Blue"
+    },
+    image:"images/ash-pants.jpg",
+    price:50
+    
+},
+
+{
+    id:7,
+    name:"Ash Pants",
+    colors:{
+        1:"Black",
+        2:"White",
+        3:"Blue"
+    },
+    image:"images/11.jpeg",
+    price:50
+    
+},
+
+{
+    id:8,
+    name:"Ash Pants",
+    colors:{
+        1:"Black",
+        2:"White",
+        3:"Blue"
+    },
+    image:"images/camo-jacket.jpg",
+    price:50
+    
+}
+
+
+]
+
+
+
+
+export default items
